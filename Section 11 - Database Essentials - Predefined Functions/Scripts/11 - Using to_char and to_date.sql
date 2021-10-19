ed5cf186c05e0df2a4332e2aa1e9bd804caa7fd8
romulovieira777 <romulo.vieira777@gmail.com>
2021-10-19 18:29:49 -0300

    Database Essentials - Predefined Functions

---
+++ Section 11 - Database Essentials - Predefined Functions/Scripts/11 - Using to_char and to_date.sql
@@ -0,0 +1,140 @@
+-- Using to_char and to_date
+SELECT
+    CURRENT_TIMESTAMP AS Current_Timestamp
+
+
+SELECT
+    CURRENT_TIMESTAMP AS Current_Timestamp
+  , TO_CHAR(CURRENT_TIMESTAMP, 'yyyy') AS Year
+  , TO_DATE('2020', 'yyyy')
+
+
+SELECT
+    CURRENT_TIMESTAMP AS Current_Timestamp
+  , TO_CHAR(CURRENT_TIMESTAMP, 'yyyy') AS Year
+
+
+SELECT
+    CURRENT_TIMESTAMP AS Current_Timestamp
+  , TO_CHAR(CURRENT_TIMESTAMP, 'yyyy') AS Year
+  , TO_DATE('20', 'yy')
+
+
+SELECT
+    CURRENT_TIMESTAMP AS Current_Timestamp
+  , TO_CHAR(CURRENT_TIMESTAMP, 'MM') AS Year
+  , TO_DATE('202011', 'yyyyMM')
+
+
+SELECT
+    CURRENT_TIMESTAMP AS Current_Timestamp
+  , TO_CHAR(CURRENT_TIMESTAMP, 'MM') AS Month
+
+
+SELECT
+    CURRENT_TIMESTAMP AS Current_Timestamp
+  , TO_CHAR(CURRENT_TIMESTAMP, 'dd') AS Year
+  , TO_DATE('20201110', 'yyyyMMdd')
+
+
+SELECT
+    CURRENT_TIMESTAMP AS Current_Timestamp
+  , TO_CHAR(CURRENT_TIMESTAMP, 'dd') AS Day_of_Month
+
+
+SELECT
+    CURRENT_TIMESTAMP AS Current_Timestamp
+  , TO_CHAR(CURRENT_TIMESTAMP, 'DD') AS Day_of_Month
+
+
+SELECT
+    CURRENT_TIMESTAMP AS Current_Timestamp
+  , TO_CHAR(CURRENT_TIMESTAMP, 'dd') AS Day_of_Month
+  , TO_CHAR(CURRENT_TIMESTAMP, 'DD') AS Day_of_Month
+
+
+SELECT
+    CURRENT_TIMESTAMP AS Current_Timestamp
+  , TO_CHAR(CURRENT_TIMESTAMP, 'DDD') AS Day_of_Year
+
+SELECT
+    CURRENT_TIMESTAMP AS Current_Timestamp
+  , TO_CHAR(CURRENT_TIMESTAMP, 'ddd') AS Day_of_Year
+  , TO_DATE('2020300', 'yyyyDDD')
+
+
+SELECT
+    CURRENT_TIMESTAMP AS Current_Timestamp
+  , TO_CHAR(CURRENT_TIMESTAMP, 'Mon') AS Month_Name
+
+
+SELECT
+    CURRENT_TIMESTAMP AS Current_Timestamp
+  , TO_CHAR(CURRENT_TIMESTAMP, 'Mon') AS Month_Name
+  , TO_DATE('2020Nov', 'yyyyMon')
+
+
+SELECT
+    CURRENT_TIMESTAMP AS Current_Timestamp
+  , TO_CHAR(CURRENT_TIMESTAMP, 'Mon') AS Month_Name
+
+
+SELECT
+    CURRENT_TIMESTAMP AS Current_Timestamp
+  , TO_CHAR(CURRENT_TIMESTAMP, 'mon') AS Month_Name
+  , TO_DATE('2020Nov', 'yyyymon')
+
+
+SELECT
+    CURRENT_TIMESTAMP AS Current_Timestamp
+  , TO_CHAR(CURRENT_TIMESTAMP, 'Month') AS Month_Name
+
+
+SELECT
+    CURRENT_TIMESTAMP AS Current_Timestamp
+  , TO_CHAR(CURRENT_TIMESTAMP, 'Month') AS Month_Name
+  , TO_DATE('2020November', 'yyyyMonth')
+
+
+SELECT
+    CURRENT_TIMESTAMP AS Current_Timestamp
+  , TO_CHAR(CURRENT_TIMESTAMP, 'month') AS Month_Name
+
+
+SELECT
+    CURRENT_TIMESTAMP AS Current_Timestamp
+  , TO_CHAR(CURRENT_TIMESTAMP, 'day') AS Day_Name
+
+
+SELECT
+    CURRENT_TIMESTAMP AS Current_Timestamp
+  , TO_CHAR(CURRENT_TIMESTAMP, 'DY') AS Day_Name
+
+
+SELECT
+    CURRENT_TIMESTAMP AS Current_Timestamp
+  , TO_CHAR(CURRENT_TIMESTAMP, 'Day') AS Day_Name
+
+
+SELECT
+    CURRENT_TIMESTAMP AS Current_Timestamp 
+  , TO_CHAR('2020-11-17'::DATE, 'Day')               AS dayname
+  , LENGTH(TO_CHAR('2020-11-17'::DATE, 'Day'))       AS dayname_length
+  , LENGTH(TRIM(TO_CHAR('2020-11-17'::DATE, 'Day'))) AS dayname_trimmed_length
+
+
+SELECT
+    CURRENT_TIMESTAMP AS Current_Timestamp 
+  , TO_CHAR('2020-11-17'::DATE, 'Day')               AS Day_Name
+  , LENGTH(TO_CHAR('2020-11-17'::DATE, 'Day'))       AS Day_Name_Length
+  , LENGTH(TRIM(TO_CHAR('2020-11-17'::DATE, 'Day'))) AS Day_Name_Trimmed_Length
+
+
+SELECT
+    CURRENT_TIMESTAMP AS Current_Timestamp
+  , TO_CHAR(CURRENT_TIMESTAMP, 'HH') AS Hour_12
+
+
+SELECT
+    CURRENT_TIMESTAMP AS Current_Timestamp
+  , TO_CHAR(CURRENT_TIMESTAMP, 'hh') AS Hour_12
